2e114d346fefcf0968560d134c56ffa84f6a8a5c
marom <maro.muszynski@gmail.com>
2019-05-01 16:29:36 +0200

    create a controller with get request for all projects

---
+++ src/main/java/com/marom/timesheet/controller/ProjectController.java
@@ -0,0 +1,23 @@
+package com.marom.timesheet.controller;
+
+import com.marom.timesheet.repository.ProjectRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProjectController {
+
+    private ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+
+    @GetMapping("/projects")
+    public String getProjects(Model model) {
+        model.addAttribute("projects", projectRepository.findAll());
+        return "projects";
+    }
+}
