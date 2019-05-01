f5fbf6e3eb3ed120a5cb51567651de39010110d6
marom <maro.muszynski@gmail.com>
2019-05-01 14:06:57 +0200

    add id generation strategy for an Employee

--- src/main/java/com/marom/timesheet/domain/Employee.java
+++ src/main/java/com/marom/timesheet/domain/Employee.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Employee {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeId;
     private Long managerEmployeeId;
     private String firstName;
