c22c0785087e9638a24ee5776a4fc86ff9c7298a
marom <maro.muszynski@gmail.com>
2019-05-01 13:34:37 +0200

    add sql insert file to load data on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into project (name, project_code) values ( 'Project 1', 'PR1' );
+insert into project (name, project_code) values ( 'Project 2', 'PR2' );
+insert into project (name, project_code) values ( 'Project 3', 'PR3' );
+insert into project (name, project_code) values ( 'Project 4', 'PR4' );
