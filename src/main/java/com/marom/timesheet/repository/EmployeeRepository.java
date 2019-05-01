63c86d737650e126f7eb337659ce069d2b713b82
marom <maro.muszynski@gmail.com>
2019-05-01 16:29:07 +0200

    fix mismatch in types in JpaRepository interface

--- src/main/java/com/marom/timesheet/repository/EmployeeRepository.java
+++ src/main/java/com/marom/timesheet/repository/EmployeeRepository.java
@@ -3,5 +3,5 @@ package com.marom.timesheet.repository;
 import com.marom.timesheet.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Long, Employee> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- src/main/java/com/marom/timesheet/repository/ProjectRepository.java
+++ src/main/java/com/marom/timesheet/repository/ProjectRepository.java
@@ -3,5 +3,5 @@ package com.marom.timesheet.repository;
 import com.marom.timesheet.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Long, Project> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
