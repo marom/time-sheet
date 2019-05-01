fdad9240084fb334660b2cb8382b05b5e3e1d94d
marom <maro.muszynski@gmail.com>
2019-05-01 13:56:00 +0200

    load Employee data on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,11 @@ insert into project (name, project_code) values ( 'Project 1', 'PR1' );
 insert into project (name, project_code) values ( 'Project 2', 'PR2' );
 insert into project (name, project_code) values ( 'Project 3', 'PR3' );
 insert into project (name, project_code) values ( 'Project 4', 'PR4' );
+
+
+insert into employee (first_name, last_name, email, employee_code) values ( 'Jon', 'Wright', 'j.wright@company.com', 'jw001' );
+insert into employee (first_name, last_name, email, employee_code) values ( 'Anna', 'Nightingale', 'a.nightingale@company.com', 'an002' );
+insert into employee (first_name, last_name, email, employee_code) values ( 'Kellen', 'Robbins', 'k.robbins@company.com', 'kr003' );
+insert into employee (first_name, last_name, email, employee_code) values ( 'Amiah', 'Dillon', 'a.dillon@company.com', 'ad004' );
+insert into employee (first_name, last_name, email, employee_code) values ( 'Barrett', 'Wolfe', 'b.wolfe@company.com', 'bw005' );
+insert into employee (first_name, last_name, email, employee_code) values ( 'Callum',  'Hubbard', 'c.hubbart@company.com', 'ch006' );
