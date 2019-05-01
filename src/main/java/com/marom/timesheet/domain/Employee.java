44471f832cf9fdfd32bf3c9d3960bd7337887cf8
marom <maro.muszynski@gmail.com>
2019-05-01 12:49:56 +0200

    add one to many relationship between Employee and Projects

--- src/main/java/com/marom/timesheet/domain/Employee.java
+++ src/main/java/com/marom/timesheet/domain/Employee.java
@@ -2,18 +2,27 @@ package com.marom.timesheet.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 public class Employee {
 
     @Id
-    private long employeeId;
-    private long managerEmployeeId;
+    @GeneratedValue
+    private Long employeeId;
+    private Long managerEmployeeId;
     private String firstName;
     private String lastName;
     private String email;
     private String employeeCode;
+
+    @OneToMany (
+            mappedBy = "employee",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Project> projects = new ArrayList<>();
 }
--- src/main/java/com/marom/timesheet/domain/Project.java
+++ src/main/java/com/marom/timesheet/domain/Project.java
@@ -2,12 +2,21 @@ package com.marom.timesheet.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Project {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String projectCode;
     private String name;
+
+    @ManyToOne (
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
 }
