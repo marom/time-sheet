f8c7febafafdd27102497baae3fbabd722c3c265
marom <maro.muszynski@gmail.com>
2019-05-01 14:12:11 +0200

    add Project repository

---
+++ src/main/java/com/marom/timesheet/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.marom.timesheet.repository;
+
+import com.marom.timesheet.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Long, Project> {
+}
