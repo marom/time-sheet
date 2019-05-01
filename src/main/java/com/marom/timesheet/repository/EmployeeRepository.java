d3d8c9dba130102844c11413d0472420a1c92eaa
marom <maro.muszynski@gmail.com>
2019-05-01 14:11:09 +0200

    add Employee repository

---
+++ src/main/java/com/marom/timesheet/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.marom.timesheet.repository;
+
+import com.marom.timesheet.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Long, Employee> {
+}
